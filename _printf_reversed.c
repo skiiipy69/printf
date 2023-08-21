8f86b6430fb1d272b0e7e9d7f28a0e3de25d0a7c
Meryem <meryemnessaf@gmail.com>
2023-08-20 22:20:08 -0700

    printf

---
+++ _printf_reversed.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _printf_reversed - prints a string, in reverse.
+ * @list: arguments.
+ * Return: string.
+ */
+int _printf_reversed(va_list list)
+{
+	int a;
+
+	char *arg;
+
+	arg = va_arg(list, char *);
+
+	for (a = 0; arg[a] != '\0'; a++)
+	{
+	}
+	a--;
+	while (arg[a] != 0)
+	{
+		_putchar(arg[a]);
+		a--;
+	}
+	return (0);
+}
+
